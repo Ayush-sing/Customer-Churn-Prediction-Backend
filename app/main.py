934e2fdb1dd24831ba99c231f6384616268daad2
Ayush-sing <118986982+Ayush-sing@users.noreply.github.com>
2026-02-10 16:57:46 +0530

    Adding Deployed Frontend URL to CORS Origins

--- app/main.py
+++ app/main.py
@@ -11,7 +11,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000",
+                   "https://customer-churn-prediction-frontend-three.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
