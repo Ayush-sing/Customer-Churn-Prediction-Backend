0bc505250d5664a29e2280a10484978ef3895cb7
Ayush-sing <118986982+Ayush-sing@users.noreply.github.com>
2026-02-11 08:10:29 +0530

    Update CORS middleware to allow only production origin
    Restrict allowed origins for CORS to the production frontend.

--- app/main.py
+++ app/main.py
@@ -11,8 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000",
-                   "https://customer-churn-prediction-frontend-three.vercel.app"],
+    allow_origins=["https://customer-churn-prediction-frontend-three.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
